ea5d2227193bd0eb13ac22a00200fa1f5588ea28
Pharaoh <anthonyadedoyin@protonmail.com>
2026-04-08 10:30:33 -0400

    Finalized Day 1 and starting Day 2

---
+++ Languages/Day01/Answer.py
@@ -0,0 +1,42 @@
+#Exercise 1
+name = "Anthony"
+age = 32
+city = "College Park"
+drink = "I do not like coffee"
+
+print(name, age, city, drink)
+
+#Exercise 2
+productName = "basketball"
+itemPrice = 3.23
+quantity = 25
+
+print(f'Total cost of all basketballs = {itemPrice * quantity}')
+
+#Exercise 3
+total_minutes = 135
+hours = total_minutes // 60
+remaining_minutes = total_minutes % 60
+
+print(f'{total_minutes} minutes becomes {hours} hours and {remaining_minutes} minutes.')
+
+#Exercise 4
+value1 = 716
+value2 = 9
+value3 = 22
+average = (value1 + value2 + value3) / 3
+
+print(average)
+
+#Exercise 5
+name = "Anthony"
+programming = "Developer"
+hoursToStudy = 7
+buildApps = True
+
+print(f'My name is {name} and I want to be a {programming}. I plan to study for {hoursToStudy} hours throughout the'
+      f'and it is {buildApps} that I want to build web applications.')
+
+#Exercise 5 redo
+print(f'My name is {name}. I want to become a {programming_interest}. I plan to study for {hours_to_study} hours '
+      f'per week. Do I want to build web applications? {wants_web_apps}.')
