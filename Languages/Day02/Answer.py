fc92d15dd1a6a0b2c61f63bd78cf483d85dcc35b
Pharaoh <anthonyadedoyin@protonmail.com>
2026-04-08 10:49:57 -0400

    Initial Submission for today

---
+++ Languages/Day02/Answer.py
@@ -0,0 +1,46 @@
+#Exercise 1
+age = 73
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+#Exercise2
+password_length = 4
+
+if password_length < 10:
+    print("Password too short")
+else:
+    print("Password length okay")
+
+#Exercise3
+order_total = 99
+is_member = False
+
+if order_total >= 50 or is_member:
+    print("Free shipping")
+else:
+    print("Shipping fee applies")
+
+#Exercise4
+temperature_c = 10
+
+if temperature_c < 0:
+    print("Frozen")
+elif 0 < temperature_c < 20:
+    print("Cold")
+else:
+    print("Warm")
+
+#Exercise5
+role = "admin"
+
+if role == "admin":
+    print("Full access")
+elif role == "editor":
+    print("Can edit content")
+elif role == "viewer":
+    print("Read-only access")
+else:
+    print("Unknown role")
