22777eb8d9ae62878aadc2e4c3f437ce573dffec
Pharaoh <anthonyadedoyin@protonmail.com>
2026-04-14 18:39:09 -0400

    Completed the questions for the typescript questions for day 6

---
+++ Languages/Day06/Answer.ts
@@ -0,0 +1,42 @@
+//question1
+const yourName: string = "Bob";
+let yourAge: number = 22;
+const buildWebApps: boolean = true;
+
+console.log(yourName, yourAge, buildWebApps);
+
+//question2
+const passwordLength: number = 8;
+
+if (passwordLength < 8) {
+    console.log("Password too short")
+}
+else {
+    console.log("Password length okay")
+}
+
+//question3
+const tasks: string[] = ["Welcome to the store", "How can I help you?", "I'm looking for a mask"]
+
+for (const task of tasks) {
+    console.log(task);
+}
+
+//question4
+function addTask(price: number, taxRate: number): number {
+    return price + (price * taxRate);
+}
+
+let result = addTask(100, .06)
+console.log(result)
+
+//question5
+const status: string = "outside";
+
+function showStatus(): void {
+    const status: string = "inside"
+    console.log(status);
+}
+
+showStatus()
+console.log(status)
