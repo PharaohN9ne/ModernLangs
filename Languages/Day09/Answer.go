c7bf063b1f6e23d3aaa31c3b577048e1dc208837
Pharaoh <anthonyadedoyin@protonmail.com>
2026-04-29 20:08:54 -0400

    Redoing Exercise 2 with } else { on the same line. Updated the loop variable tasks so it does not shadow the slice name tasks.

--- Languages/Day09/Answer.go
+++ Languages/Day09/Answer.go
@@ -2,7 +2,7 @@ package Day09
 
 import "fmt"
 
-//Question 1
+// Question 1
 func main1() {
 	name := "Greg"
 	age := 66
@@ -11,39 +11,38 @@ func main1() {
 	fmt.Println(name, age, buildWebApps)
 }
 
-//Question 2
+// Question 2
 func main2() {
 	var passwordLength int
 
 	if passwordLength < 8 {
 		fmt.Println("Password too short")
-	}
-	else {
+	} else {
 		fmt.Println("Password length okay")
 	}
 }
 
-//Question 3
+// Question 3
 func main3() {
-	tasks := []string {"Take me to lunch", "grab dinner with the homie", "Buy sandwich"}
+	tasks := []string{"Take me to lunch", "grab dinner with the homie", "Buy sandwich"}
 
-	for index, tasks := range tasks {
-		fmt.Println(index, tasks)
+	for index, task := range tasks {
+		fmt.Println(task)
 	}
 }
 
-//Question 4
-func addTax (price float64, taxRate float64) float64 {
+// Question 4
+func addTax(price float64, taxRate float64) float64 {
 	return price + (price * taxRate)
 }
 
-func main4()  {
+func main4() {
 	finalPrice := addTax(98.00, .08)
 
 	fmt.Println(finalPrice)
 }
 
-//Question 5
+// Question 5
 func main5() {
 	status := "outside"
 
