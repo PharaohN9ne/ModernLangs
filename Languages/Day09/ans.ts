5f2fc43d8509a8d4add04ef332ba2a10afe2452e
Pharaoh <anthonyadedoyin@protonmail.com>
2026-05-13 08:31:25 -0400

    Folder name update | day9 question and initial ans

---
+++ Languages/Day09/ans.ts
@@ -0,0 +1,39 @@
+// question 1
+
+const favoriteFoods: string[] = ["pizza", "donuts", "cookies"];
+console.log(favoriteFoods);
+console.log(favoriteFoods[0]);
+console.log(favoriteFoods[2]);
+
+// question 2
+
+const userProfile: [string, number, boolean] = ["bob", 39.0, true];
+console.log(userProfile);
+
+//question 3
+
+const book = {
+    title: "Apple",
+    author: "steve jobs",
+    pages: 549,
+};
+
+console.log(book);
+console.log(book.title);
+
+// question 4
+
+const skills = new Set(["boxing", "Muay Thai", "karate", "Muay Thai"]);
+console.log(skills);
+console.log(skills.has("boxing"));
+
+// question 5
+
+const student = {
+    name: "mike",
+    languages: ["English", "French", "German"],
+    active: true
+}
+
+console.log(student);
+console.log(student.languages);
