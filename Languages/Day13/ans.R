8dad5d8f0f6312b4a43c1fa2fd4fe84058d01be8
Pharaoh <anthonyadedoyin@protonmail.com>
2026-05-19 09:54:38 -0400

    Week 2 R q&a | initial commit

---
+++ Languages/Day13/ans.R
@@ -0,0 +1,38 @@
+#question 1
+
+favorite_foods <- c("apple", "papaya", "dragonfruit")
+print(favorite_foods)
+print(favorite_foods[1])
+print(favorite_foods[3])
+
+#question 2
+
+user_profile <- list("Bob", 45, FALSE)
+print(user_profile)
+
+#question 3
+
+book <- list(
+  title = "Apple",
+  author = "Steve Jobs",
+  pages = 549
+)
+
+print(book)
+print(book$title)
+
+#question 4
+
+skills <- factor(c("writor", "editor", "director", "photographer", "director"))
+print(roles)
+
+#question 5
+
+students <- data.frame(
+  name = c("Mike", "Ava", "Jordan"),
+  languages = c("English", "French", "Spanish"),
+  active = c(TRUE, TRUE, FALSE)
+)
+
+print(students)
+print(students$languages)
